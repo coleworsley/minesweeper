b9ccd300cd241cf74ce8c158252da30a3e9ab33b
coleworsley <cole.s.worsley@gmail.com>
2018-11-10 13:29:52 -0700

    add initial components

---
+++ src/components/Box.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Box extends Component {
+  render() {
+    return (
+      <div className="box">
+        <p>test</p>
+      </div>
+    )
+  }
+}
+
+export default Box;
---
+++ src/components/Grid.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Grid extends Component {
+  render() {
+    return (
+      <div className="grid">
+        <p>test</p>
+      </div>
+    )
+  }
+}
+
+export default Grid;
